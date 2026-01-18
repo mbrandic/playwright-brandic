776c2b793a545dca9a2297bcafafae2203ce8520
mbrandic <148384198+mbrandic@users.noreply.github.com>
2026-01-18 17:21:46 +0100

    Dodala POM i 5 testova

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -5,28 +5,18 @@ test.describe('Košarica – dodavanje i brisanje proizvoda', () => {
 
   test('Dodavanje proizvoda u košaricu', async ({ page }) => {
     const cartPage = new CartPage(page);
-
-    // Otvaranje proizvoda i dodavanje u košaricu
     await cartPage.openProductPage();
     await cartPage.addProductToCart();
-
-    // Otvaranje košarice i provjera
     await cartPage.openCartPage();
     await cartPage.verifyProductIsInCart();
   });
 
   test('Brisanje proizvoda iz košarice', async ({ page }) => {
     const cartPage = new CartPage(page);
-
-    // Prvo dodajemo proizvod da bismo ga mogli obrisati
     await cartPage.openProductPage();
     await cartPage.addProductToCart();
-
-    // Otvaramo košaricu i provjeravamo proizvod
     await cartPage.openCartPage();
     await cartPage.verifyProductIsInCart();
-
-    // Brišemo proizvod i provjeravamo praznu košaricu
     await cartPage.removeProductFromCart();
     await cartPage.verifyCartIsEmpty();
   });
